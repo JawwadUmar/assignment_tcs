8d7a74a3d1927b67610e208cccc666d4a54ada28
Jawwad Umar <jawwadumar99@gmail.com>
2025-09-24 10:47:42 +0000

    more of commnts added

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -9,6 +9,8 @@ const UserProfile: React.FC<Props> = ({ userId }) => {
   //A proper user type is not defined over here
   //The use of 'any' keyword overlooks typesafety(defeats the purpose of Typescript)
 
+  //SECURITY CONCERN: URL is hardcoded, could expose internal ENDPOINTS
+  //Use environment variables (process.env.REACT_APP_APIURL)
   useEffect(() => {
     fetch(`https://api.example.com/user/${userId}`)
       .then(response => response.json())
@@ -17,6 +19,7 @@ const UserProfile: React.FC<Props> = ({ userId }) => {
   }, []);  //Here the useEffect does not include anything in it s dependency array
   //It should have included userId here (as userId changes the effect should re-run)
   //Also there is no cancellation logic in useEffect (If multiple fetches ,overlap, the state may be updated incorrectly)
+  //Use AbortController!
 
   return (
     <div>
